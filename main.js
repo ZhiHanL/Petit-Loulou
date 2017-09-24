9d5297a77ed4233ed42c7fa56f05f6e8765d7b8e
Lupe1o1 <Lupe1o1@users.noreply.github.com>
2017-09-24 10:14:56 -0700

    changed mobile spacing

--- main.js
+++ main.js
@@ -1,62 +1,56 @@
 var game;
-var useLandscape=true;
+var useLandscape = true;
 
-window.onload = function () {
+window.onload = function() {
 
-var isMobile=navigator.userAgent.indexOf("Mobile");
-var gameWidth = window.innerWidth*0.80;
-var gameHeight = window.innerHeight*0.4;
-if (isMobile>-1)
-     {
-        isMobile=true;
-     }
-     else
-     {
-        isMobile=false;
-     }
+  var isMobile = navigator.userAgent.indexOf("Mobile");
+  var gameWidth = window.innerWidth * 0.80;
+  var gameHeight = window.innerHeight * 0.4;
+  if (isMobile > -1) {
+    isMobile = true;
+  } else {
+    isMobile = false;
+  }
 
-    if (isMobile==false) {
-        //desktop laptop
-        if (useLandscape == true) {
-            game = new Phaser.Game(window.innerWidth*0.80, window.innerHeight*0.4, Phaser.AUTO, "ph_game");
-        } else {
-
-            game = new Phaser.Game(480, 640, Phaser.AUTO, "ph_game");
-        }
-
+  if (isMobile == false) {
+    //desktop laptop
+    if (useLandscape == true) {
+      game = new Phaser.Game(window.innerWidth * 0.80, window.innerHeight * 0.4, Phaser.AUTO, "ph_game");
     } else {
-        //mobile device
-        game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, "ph_game");
-    }
-	
-var seal;
-var StateMain={    
-	
-   preload:function()
-    {
-       game.load.image('test', 'assets/Sebastian.jpg');
-    },
-    
-    create:function()
-    {
-		seal = createSeal(game.world.centerX,game.world.centerY);
-    },
-    
-    update:function()
-    {       
-        
+
+      game = new Phaser.Game(480, 640, Phaser.AUTO, "ph_game");
     }
 
-    
-}
+  } else {
+    //mobile device
+    game = new Phaser.Game(window.innerWidth * 0.80, window.innerHeight * 0.4, Phaser.AUTO, "ph_game");
+  }
 
-function createSeal(x,y){
-		var seal = game.add.sprite(x,y,'test');
-		seal.anchor.setTo(0.5,0.5);
-		//seal.height = gameHeight; 
-		return seal;
-	}
+  var seal;
+  var StateMain = {
 
-    game.state.add("StateMain",StateMain);
-    game.state.start("StateMain");
+    preload: function() {
+      game.load.image('test', 'assets/Sebastian.jpg');
+    },
+
+    create: function() {
+      seal = createSeal(game.world.centerX, game.world.centerY);
+    },
+
+    update: function() {
+
+    }
+
+
+  }
+
+  function createSeal(x, y) {
+    var seal = game.add.sprite(x, y, 'test');
+    seal.anchor.setTo(0.5, 0.5);
+    //seal.height = gameHeight;
+    return seal;
+  }
+
+  game.state.add("StateMain", StateMain);
+  game.state.start("StateMain");
 }
